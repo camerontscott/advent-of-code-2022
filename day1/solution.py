65c7d18ad6c4949ae784b663c32cee1f10362087
Cameron Scott <cameronscott@Camerons-MacBook-Pro.local>
2022-12-01 17:44:02 -0600

    [day1] read input from file

---
+++ day1/solution.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+This list represents the Calories of the food carried by five Elves:
+
+The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+The second Elf is carrying one food item with 4000 Calories.
+The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+The fifth Elf is carrying one food item with 10000 Calories.
+
+"""
+
+# open text file in read mode
+text_file = open("./input.txt", "r")
+
+#read whole file to a string
+data = text_file.read()
+
+#close file
+text_file.close()
+
+print(data)
