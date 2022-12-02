8c1e11ee333adb524414d66ecf811deb28cd3069
Cameron Scott <cameronscott@Camerons-MacBook-Pro.local>
2022-12-01 22:58:35 -0600

    refactor and solve day 1 pt 2

--- day1/solution.py
+++ day1/solution.py
@@ -23,26 +23,37 @@ The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 2
 The fifth Elf is carrying one food item with 10000 Calories.
 
 """
-
+def prepare_input():
 # open text file in read mode
-text_file = open("/Users/cameronscott/Dev/advent-of-code-2022/day1/input.txt", "r")
+    text_file = open("/Users/cameronscott/Dev/advent-of-code-2022/day1/input.txt", "r")
 
-#read whole file to a string
-data = text_file.read()
+    #read whole file to a string
+    data = text_file.read()
 
-#close file
-text_file.close()
+    #close file
+    text_file.close()
 
-each_elf_calories = data.split("\n\n")
+    each_elf_calories = data.split("\n\n")
+    return each_elf_calories
 
 
 
-max = 0
 
-for calorie_counts in each_elf_calories:
-    sum = 0
-    for calorie_count in calorie_counts.split("\n"):
-        sum = sum + int(calorie_count)  
-    if sum > max:
-        max = sum
-print(max)        
+def find_nax_sum():
+    top_three_sum = 0
+    maximums = []
+    max = 0
+    for calorie_counts in prepare_input():
+        # print(calorie_counts)
+        sum = 0
+        for calorie_count in calorie_counts.split("\n"):
+            sum = sum + int(calorie_count)  
+            if sum > max:
+                max = sum
+                maximums.append(max)
+    top_three_sum = top_three_sum + maximums[-3]
+    top_three_sum = top_three_sum + maximums[-2]
+    top_three_sum = top_three_sum + maximums[-1]
+    print(top_three_sum)
+
+find_nax_sum()
