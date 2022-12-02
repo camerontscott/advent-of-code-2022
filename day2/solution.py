0ec02a3606067fd0f4560246961967f1f58db739
Cameron Scott <cameronscott@Camerons-MacBook-Pro.local>
2022-12-01 23:56:03 -0600

    [day2] solve day2 pt 1

--- day2/solution.py
+++ day2/solution.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+
+def prepare_input():
+# open text file in read mode
+    text_file = open("/Users/cameronscott/Dev/advent-of-code-2022/day2/input.txt", "r")
+
+    data = text_file.read()
+
+    #close file
+    text_file.close()
+    lines = data.split('\n')
+
+    return lines
+
+def calculate_score(input):
+    score = 0
+    for line in input:
+        if (line[2] == 'X'):
+            score = score + 1
+            if(line[0] == 'A'):
+                score = score + 3
+            if(line[0] == 'C'):
+                score = score + 6
+        if (line[2] == 'Y'):
+            score = score + 2
+            if(line[0] == 'A'):
+                score = score + 6
+            if(line[0] == 'B'):
+                score = score + 3
+        if (line[2] == 'Z'):
+            score = score + 3
+            if(line[0] == 'B'):
+                score = score + 6
+            if(line[0] == 'C'):
+                score = score + 3
+    print(score)
+
+
+calculate_score(prepare_input())
