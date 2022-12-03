2b8de858e864602da25c95f5dda8b97c83061448
Cameron Scott <cameronscott@Camerons-MacBook-Pro.local>
2022-12-02 23:56:42 -0600

    [day3] solve day3 pt 1

--- day3/solution.py
+++ day3/solution.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+def prepare_input():
+# open text file in read mode
+    text_file = open("/Users/cameronscott/Dev/advent-of-code-2022/day3/input.txt", "r")
+
+    data = text_file.read()
+
+    #close file
+    text_file.close()
+    lines = data.split('\n')
+
+    return lines
+
+def map_priority(character):
+    if character.islower():
+        return ord(character) - 96
+    else:
+        return ord(character) - 38
+
+def solve(lines):
+    sum = 0
+    for line in lines:
+        my_set = set(line[:len(line)//2])
+        second_half = line[len(line)//2:]
+        common_item = list(my_set.intersection(second_half))[0]
+        sum = sum + map_priority(common_item)
+    print(sum)
+solve(prepare_input())
