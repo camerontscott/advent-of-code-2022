850efa08582a976096df9354eaac3888f359747e
Cameron Scott <cameronscott@Camerons-MacBook-Pro.local>
2022-12-03 00:33:45 -0600

    [day3] solve pt2

--- day3/solution.py
+++ day3/solution.py
@@ -17,7 +17,7 @@ def map_priority(character):
     else:
         return ord(character) - 38
 
-def solve(lines):
+def solve_p1(lines):
     sum = 0
     for line in lines:
         my_set = set(line[:len(line)//2])
@@ -25,4 +25,19 @@ def solve(lines):
         common_item = list(my_set.intersection(second_half))[0]
         sum = sum + map_priority(common_item)
     print(sum)
-solve(prepare_input())
+
+
+def solve_p2(lines):
+    sum = 0
+    for i in range(0, len(lines)-2, 3):
+        s1 = set(lines[i])
+        s2 = set(lines[i+1])
+        s3 = set(lines[i+2])
+      
+        set1 = s1.intersection(s2)
+        common_between_three = list(set1.intersection(s3))[0]
+        sum = sum + map_priority(common_between_three)
+    print(sum)
+
+solve_p1(prepare_input())
+solve_p2(prepare_input())
